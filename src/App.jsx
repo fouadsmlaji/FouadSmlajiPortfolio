2482ac218214aaf6c3874a3b82e6dc772396a2ef
foodz66 <138889360+foodz66@users.noreply.github.com>
2025-02-20 10:46:27 +0300

    main app fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./Components/Header.css";
 import "./KeyFrames.css";
 import "./Media.css";
 import HomePage from "./Pages/HomePage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import Projects from "./Pages/Projects";
 import ProjectComponent from "./Components/ProjectComponent";
@@ -13,12 +13,14 @@ function App() {
   return (
     <>
       {/* {location.pathname === "/" && <Header />} */}
+      <BrowserRouter basename="/FouadSmlajiPortfolio">
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/tour" element={<LandingPage />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/projects/:id" element={<ProjectComponent />}></Route>x
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tour" element={<LandingPage />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<ProjectComponent />} />
       </Routes>
+    </BrowserRouter>
     </>
   );  
 }
