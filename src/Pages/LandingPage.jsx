82f63dee40b350f9cc767b6a10d9313dde957bc3
Your Name <fouad.smlaji66@gmail.com>
2026-01-12 10:42:21 +0300

    Enhance LandingPage and HomePage styles and content. Updated LandingPage to reflect a Full Stack Developer profile, added Express and PostgreSQL icons, and improved layout with new CSS styles. Adjusted margins and font sizes for better visual alignment.

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -8,6 +8,8 @@ import Html from "../../public/assets/images/Icons/Html.svg";
 import Css from "../../public/assets/images/Icons/Css.svg";
 import Javascript from "../../public/assets/images/Icons/Javascript.svg";
 import Sass from"../../public/assets/images/Icons/Sass.svg";
+import Express from "../../public/assets/images/Icons/expressjs-icon.svg";
+import PostgreSQL from "../../public/assets/images/Icons/Postgresql_elephant.svg";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -36,6 +38,8 @@ export default function LandingPage() {
       <div className="LandingPageContainer">
         <img src={EarthImage} alt="" className="EarthImage2" style={proceededStyle ?  style : {}}/>
         <div className="d-flex flex-row align-items-center justify-content-between iconsDiv " style={proceededStyle ?  fadeStyle : {}}>
+        <img src={Express} alt="" height={60} className="expressIcon" />
+        <img src={PostgreSQL} alt="" height={60} />
           <img src={ReactIcon} alt="" height={60} />
           <img src={Vue} alt="" height={60} />
           <img src={Git} alt="" height={60} />
@@ -45,23 +49,15 @@ export default function LandingPage() {
           <img src={Html} alt="" height={60} />
           <img src={Css} alt="" height={60} />
           <img src={Javascript} alt="" height={60} />
+          
         </div>
         <div className="d-flex flex-row align-items-center justify-content-center row w-100" style={proceededStyle ?  fadeStyle : {}}>
           <div className="col-6 col-lg-6 h-100 hideDiv"></div>
           <div className="col-12 col-lg-6 d-flex flex-column align-items-center justify-content-center h-100">
             <div className="pt-4">
-              <h2 className="aboutTitle">Frontend Developer</h2>
+              <h2 className="aboutTitle">Full Stack Developer</h2>
               <p className="aboutText">
-                I am a frontend developer with a strong focus on React,
-                dedicated to building high-performance, user-friendly web
-                applications. With expertise in modern frontend technologies, I
-                specialize in developing responsive and scalable interfaces that
-                enhance user experience and engagement. My approach combines
-                clean, maintainable code with industry best practices to ensure
-                optimal functionality, accessibility, and performance. I am
-                committed to continuous learning and staying up to date with
-                emerging technologies to deliver innovative and efficient
-                solutions.
+              Results-driven Software Engineer with over 3 years of experience in full-stack web development. Skilled in building responsive, scalable web applications using React.js, Vue.js, WordPress, and modern backend technologies such as Express.js. Experienced in improving website performance, enhancing SEO, and delivering high-quality client projects. Passionate about creating impactful digital experiences and optimizing development workflows.
               </p>
               <button className="btn1">
                 <a href="/Fouad Smlaji Resume.pdf" download="Fouad_Smlaji_Resume.pdf">Resume</a>
